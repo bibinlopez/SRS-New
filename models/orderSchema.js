2019f1298c83ac4fc89756bb1bff364a5da80910
Bibin Lopez <bibinlopez36@gmail.com>
2023-06-21 13:06:57 +0530

    order schema

---
+++ models/orderSchema.js
@@ -0,0 +1,50 @@
+
+const mongoose = require('mongoose')
+
+const SingleOrderItems = mongoose.Schema({
+   name: { type: String, required: true },
+   Image: { type: String, required: true },
+   price: { type: String, required: true },
+   quantity: { type: Number, required: true },
+   product: {
+      type: mongoose.Types.ObjectId,
+      ref: 'Product',
+      required: true,
+   }
+})
+
+const OrderSchema = mongoose.Schema({
+   shippingFee: {
+      type: Number,
+      required: true,
+   },
+   subtotal: {
+      type: Number,
+      required: true,
+   },
+   total: {
+      type: Number,
+      required: true,
+   },
+   orderItems: [SingleOrderItems],
+   status: {
+      type: String,
+      enum: ['pending', 'failed', 'paid', 'delivered', 'canceled'],
+      default: 'pending'
+   },
+   user: {
+      type: mongoose.Types.ObjectId,
+      ref: 'User',
+      required: true,
+   },
+   clientSecret: {
+      type: String
+   },
+   paymentIntendId: {
+      type: String
+   },
+}, { timestamps: true })
+
+
+
+module.exports = mongoose.Schema('Order', OrderSchema)
