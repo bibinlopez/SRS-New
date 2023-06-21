45b3fd606b7c4ad5bac7efb0a26db58b01cdbe8f
Bibin Lopez <bibinlopez36@gmail.com>
2023-06-21 10:55:30 +0530

    get products

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,21 +1,26 @@
 
 const Product = require('../models/productSchema')
-const CustomAPIError = require('../errors/custom-error')
+const { CustomAPIError } = require('../errors/custom-error')
 
 const createProduct = async (req, res) => {
    req.body.user = req.user.userId;
    const product = new Product(req.body)
    const result = await product.save()
-   return res.status(201).json({ result })
+   return res.status(201).json({ data: result })
 }
 
 const getAllProduct = async (req, res) => {
-   res.send('get all  product')
+   const product = await Product.find({})
+   return res.status(200).json({ data: product, count: product.length })
 }
 
 
 const getSingleProduct = async (req, res) => {
-   res.send('get single product')
+   const product = await Product.findById(req.params.id)
+   if (!product) {
+      throw new CustomAPIError(`No product with the id: ${req.params.id}`, 404)
+   }
+   return res.status(200).json({ data: product })
 }
 
 const updateProduct = async (req, res) => {
