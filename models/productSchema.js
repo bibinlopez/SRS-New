7eb932d2ca276497451140257241a08cfa47d5b1
Bibin Lopez <bibinlopez36@gmail.com>
2023-08-07 15:26:16 +0530

    category enum removed

--- models/productSchema.js
+++ models/productSchema.js
@@ -1,71 +1,70 @@
-
 const mongoose = require('mongoose')
 
-const ProductSchema = new mongoose.Schema({
-   name: {
+const ProductSchema = new mongoose.Schema(
+  {
+    name: {
       type: String,
       trim: true,
       required: [true, 'Please provide product name'],
-      maxlength: [100, 'Nmae can not be more than 100 characters']
-   },
-   price: {
+      maxlength: [100, 'Nmae can not be more than 100 characters'],
+    },
+    price: {
       type: Number,
       required: [true, 'Please provide product price'],
-      default: 0
-   },
-   description: {
+      default: 0,
+    },
+    description: {
       type: String,
       required: [true, 'Please provide product name'],
-      maxlength: [1000, 'Nmae can not be more than 100 characters']
-   },
+      maxlength: [1000, 'Nmae can not be more than 100 characters'],
+    },
 
-   image: {
+    image: {
       type: String,
-      default: 'https://res.cloudinary.com/dzoy3xghm/image/upload/v1687270612/default-product-image_ysgdll.png'
-   },
-   category: {
+      default:
+        'https://res.cloudinary.com/dzoy3xghm/image/upload/v1687270612/default-product-image_ysgdll.png',
+    },
+    category: {
       type: String,
       required: [true, 'Please provide product category'],
-      enum: ['office', 'kitchen', 'bedroom'],
-   },
-   company: {
+    },
+    company: {
       type: String,
       required: [true, 'Please provide company'],
       enum: {
-         values: ['ikea', 'liddy', 'marcos'],
-         massage: '{VALUE} is not supported'
-      }
-   },
-   colors: {
+        values: ['ikea', 'liddy', 'marcos'],
+        massage: '{VALUE} is not supported',
+      },
+    },
+    colors: {
       type: [String],
       default: ['#222'],
-      required: true
-   },
-   featured: {
+      required: true,
+    },
+    featured: {
       type: Boolean,
       default: false,
-   },
-   freeShipping: {
+    },
+    freeShipping: {
       type: Boolean,
-      default: false
-   },
-   inventory: {
+      default: false,
+    },
+    inventory: {
       type: Number,
       required: true,
       default: 15,
-   },
-   averageRating: {
+    },
+    averageRating: {
       type: Number,
-      default: 0
-   },
-   admin: {
+      default: 0,
+    },
+    admin: {
       type: mongoose.Types.ObjectId,
       ref: 'User',
-      required: true
-   }
-}, { timestamps: true })
-
-
-
+      required: true,
+    },
+  },
+  { timestamps: true }
+)
 
 module.exports = mongoose.model('Product', ProductSchema)
